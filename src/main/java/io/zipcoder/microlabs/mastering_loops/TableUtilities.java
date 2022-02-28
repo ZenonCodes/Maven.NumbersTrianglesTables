6d41f63735149e03cf3d0f22b4e3fe4dc72057bd
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-28 18:03:26 -0500

    Implemented first bout of this Table TC is not happy

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,26 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(4);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(9);
+
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for (int i = 1; i < tableSize + 1; i++){
+            result += i;
+            for (int j = i + 1; j < tableSize + 1; j++) {
+                result += j + " | ";
+            }
+            result += '\n';
+        }
+        return result;
     }
 }
